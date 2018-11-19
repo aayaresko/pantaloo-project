85b6b81b4a0cffbe35ba86d6acaa2813faecbdf2
alexproc <alexproc1313@gmail.com>
2018-11-19 17:04:09 +0200

    add testing client

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function test(Request $request)
     {
+        $client = new Client();
+        $result = $client->post('https://stage.game-program.com/api/seamless/provider', [
+            'form_params' => [
+                'api_password' => 'casinobit_mc_s',
+                'api_login' => 'SPHhcXLHSZyg28OlpY',
+                'method' => 'getGameList',
+                'show_systems' => 0,
+                'currency' => 'EUR',
+            ]
+        ]);
+        dd($result);
        return view('testtest');
     }
 }
