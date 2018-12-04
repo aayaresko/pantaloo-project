9c2e856c978a8167baac5fd461c035a9a4c312cb
alexproc <alexproc1313@gmail.com>
2018-12-04 16:15:06 +0200

    add iframe

--- resources/views/load/integrated_games_link.blade.php
+++ resources/views/load/integrated_games_link.blade.php
@@ -1,1 +1,3 @@
-<iframe src="{{ $link }}" width="960" height="620"></iframe>
+<iframe src="{{ $link }}" width="960" height="620" allowtransparency="true" allowfullscreen="true"
+        webkitallowfullscreen="true" mozallowfullscreen="true" allowFullScreen="true" frameborder="0" scrolling="no">
+</iframe>
