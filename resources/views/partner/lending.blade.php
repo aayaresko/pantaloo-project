ae5ddeec5d46a4a50d7b16c685fca48e88f9bdeb
webmaxstudio <root@casinobit.ru>
2018-12-06 06:00:16 -0500

    add style for leanding

--- resources/views/partner/lending.blade.php
+++ resources/views/partner/lending.blade.php
@@ -9,21 +9,21 @@
 
     <!-- Favicons
         ================================================== -->
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+    <link rel="shortcut icon" href="/partner/img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="/partner/img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/partner/img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/partner/img/apple-touch-icon-114x114.png">
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css"  href="/partner/css/bootstrap.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="/partner/fonts/font-awesome/css/font-awesome.css">
 
     <!-- Stylesheet
         ================================================== -->
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/nivo-lightbox/nivo-lightbox.css">
-    <link rel="stylesheet" type="text/css" href="css/nivo-lightbox/default.css">
+    <link rel="stylesheet" type="text/css" href="/partner/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/partner/css/nivo-lightbox/nivo-lightbox.css">
+    <link rel="stylesheet" type="text/css" href="/partner/css/nivo-lightbox/default.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800,900" rel="stylesheet">
@@ -43,7 +43,7 @@
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-            <a class="navbar-brand page-scroll" href="#page-top" style="padding: 0 !important;"><img src="img/logo-affiliate-casinobit.png" style="width: 230px; height: auto;" /></a> </div>
+            <a class="navbar-brand page-scroll" href="#page-top" style="padding: 0 !important;"><img src="/partner/img/logo-affiliate-casinobit.png" style="width: 230px; height: auto;" /></a> </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -81,7 +81,7 @@
 <div id="about">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-md-6"> <img src="img/about.jpg" class="img-responsive" alt=""> </div>
+            <div class="col-xs-12 col-md-6"> <img src="/partner/img/about.jpg" class="img-responsive" alt=""> </div>
             <div class="col-xs-12 col-md-6">
                 <div class="about-text">
                     <h2>About Us</h2>
@@ -243,7 +243,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-12 col-md-6"> <img src="img/commissions.png" class="img-responsive" alt=""> </div>
+            <div class="col-xs-12 col-md-6"> <img src="/partner/img/commissions.png" class="img-responsive" alt=""> </div>
         </div>
     </div>
 </div>
@@ -313,12 +313,12 @@
         <p>&copy; 2018 CasinoBit Affiliates. Design by <a href="https://partners.casinobit.io" rel="nofollow">US</a></p>
     </div>
 </div>
-<script type="text/javascript" src="js/jquery.1.11.1.js"></script>
-<script type="text/javascript" src="js/bootstrap.js"></script>
-<script type="text/javascript" src="js/SmoothScroll.js"></script>
-<script type="text/javascript" src="js/nivo-lightbox.js"></script>
-<script type="text/javascript" src="js/jqBootstrapValidation.js"></script>
-<script type="text/javascript" src="js/contact_me.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
+<script type="text/javascript" src="/partner/js/jquery.1.11.1.js"></script>
+<script type="text/javascript" src="/partner/js/bootstrap.js"></script>
+<script type="text/javascript" src="/partner/js/SmoothScroll.js"></script>
+<script type="text/javascript" src="/partner/js/nivo-lightbox.js"></script>
+<script type="text/javascript" src="/partner/js/jqBootstrapValidation.js"></script>
+<script type="text/javascript" src="/partner/js/contact_me.js"></script>
+<script type="text/javascript" src="/partner/js/main.js"></script>
 </body>
 </html>
