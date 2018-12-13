bd5bc05c9a702963585cb37fbf43792698ee4150
alexproc <alexproc1313@gmail.com>
2018-12-13 13:00:47 +0200

    add auth for partner

---
+++ app/Http/Controllers/Auth/Affiliates/AuthController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Affiliates;
+
+use App\Bitcoin\Service;
+use App\Currency;
+use App\Jobs\SetUserCountry;
+use App\Tracker;
+use App\User;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Support\Facades\Cookie;
+
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins, DispatchesJobs;
+
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+    protected $loginPath = '/';
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        $service = new Service();
+        $address = $service->getNewAddress('common');
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        $user->bitcoin_address = $address;
+        $user->balance = 0;
+
+        if(isset($_SERVER['HTTP_CF_CONNECTING_IP']))
+        {
+            $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+            $user->ip = $ip;
+        }
+
+        $tracker_id = Cookie::get('tracker_id');
+
+        if($tracker_id) {
+            $tracker = Tracker::find($tracker_id);
+
+            if($tracker)
+            {
+                $user->tracker()->associate($tracker);
+                $user->agent_id = $tracker->user_id;
+            }
+        }
+
+        $currency = Currency::find($data['currency']);
+
+        if($currency)
+        {
+            $user->currency_id = $currency->id;
+        }
+
+        $user->save();
+
+        $this->dispatch(new SetUserCountry($user));
+
+        return $user;
+    }
+
+    public function share()
+    {
+
+    }
+}
