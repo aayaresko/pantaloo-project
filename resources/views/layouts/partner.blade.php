c8786934f156b4e38df5b9528d9ae3424caba874
alexproc <alexproc1313@gmail.com>
2018-12-13 15:25:33 +0200

    add link for leanding

--- resources/views/layouts/partner.blade.php
+++ resources/views/layouts/partner.blade.php
@@ -44,7 +44,7 @@
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-            <a class="navbar-brand page-scroll" href="#page-top" style="padding: 0 !important;"><img src="/partner/img/logo-affiliate-casinobit.png" style="width: 230px; height: auto;" /></a> </div>
+            <a class="navbar-brand page-scroll" href="/" style="padding: 0 !important;"><img src="/partner/img/logo-affiliate-casinobit.png" style="width: 230px; height: auto;" /></a> </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
