153108ae9f8a7daca59e1ab6ac165e5e515622c4
alexproc <alexproc1313@gmail.com>
2018-12-18 13:03:46 +0200

    add create filed transactiob

--- app/Transaction.php
+++ app/Transaction.php
@@ -9,7 +9,7 @@ class Transaction extends Model
 {
     use SoftDeletes;
 
-    protected $fillable = ['sum', 'type', 'user_id', 'round_id', 'comment'];
+    protected $fillable = ['sum', 'agent_id', 'agent_commission', 'type', 'user_id', 'round_id', 'comment'];
 
     protected $dates = ['deleted_at'];
 
