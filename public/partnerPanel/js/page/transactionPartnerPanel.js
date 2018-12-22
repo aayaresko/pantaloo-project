347b739247c96c6fd01c85d11e70fca67d585ff9
alexproc <alexproc1313@gmail.com>
2018-12-22 15:08:54 +0200

    parners transactions js

--- public/partnerPanel/js/page/transactionPartnerPanel.js
+++ public/partnerPanel/js/page/transactionPartnerPanel.js
@@ -3,36 +3,40 @@
 $(document).ready(function () {
 
     let options = {};
-    options.push({name: "user_id", value: $('select[name="user_id"]').val()});
-    options.push({name: "category_id", value: $('select[name="category_id"]').val()});
-    options.push({name: "type_id", value: $('select[name="type_id"]').val()});
+    function setOptions() {
+        options.user_id = $('select[name="user_id"]').val();
+        options.category_id = $('select[name="category_id"]').val();
+        options.type_id = $('select[name="type_id"]').val();
+    }
 
-    let table = $('.datatable').DataTable({
-        "order": [[0, "asc"]],
-        "processing": true,
-        "serverSide": true,
-        "ajax": {
-            "url": transactionRoute,
-            "dataType": "json",
-            "type": "GET",
-            "data": options,
-            "error": function (xhr, error, thrown) {
-                if (statusError === 0) {
-                    statusError++;
-                    //table.ajax.reload()
-                }
-            }
-        },
-        "columns": [
-            {"data": "id"},
-            {"data": "created_at"},
-            {"data": "description"},
-            {"data": "sum"},
-            {"data": "bonus_sum"},
-        ]
+    function initDataTable() {
+        $('#tableOrder').DataTable().destroy();
+        let table = $('.datatable').DataTable({
+            "order": [[0, "asc"]],
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": transactionRoute,
+                "dataType": "json",
+                "type": "GET",
+                "data": options,
+            },
+            "columns": [
+                {"data": "id"},
+                {"data": "created_at"},
+                {"data": "description"},
+                {"data": "sum"},
+                {"data": "bonus_sum"},
+            ]
+        });
+    }
+
+    $(function () {
+        initDataTable();
     });
 
     $('.selectpicker').change(function () {
-        table.fnDraw();
+        setOptions();
+        initDataTable();
     });
 });
