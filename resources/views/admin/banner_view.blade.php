a0a72f1138cab32581f197286c0a307377cd0168
Vlad <rulessweb@gmail.com>
2018-12-28 10:53:08 +0200

    Table responcive

--- resources/views/admin/banner_view.blade.php
+++ resources/views/admin/banner_view.blade.php
@@ -6,30 +6,31 @@
                 <div class="col-sm-12">
                     <div class="card-box">
                         @include('agent.errors')
-
-                        <table class="table table-hover">
-                            <tr>
-                                <th>Thumbnail</th>
-                                <th>File Info</th>
-                                <th>Actions</th>
-                            </tr>
-
-                            @foreach($banners as $banner)
+                        <div class="table-wrap">
+                            <table class="table table-hover">
                                 <tr>
-                                    <td><img src="{{$banner->url}}" style="max-height: 100px;"></td>
-                                    <td>
-                                        <b>Size:</b> {{$banner->size}}<br>
-                                        <b>Type:</b> {{$banner->type}}
-                                    </td>
-                                    <td>
-                                        <a href="{{$banner->url}}" class="btn btn-success" download="{{$banner->id}}">Download</a>
-                                        @if(Auth::user()->isAdmin())
-                                            <a href="{{route('banners.delete', $banner)}}" class="btn btn-danger">DELETE</a>
-                                        @endif
-                                    </td>
+                                    <th>Thumbnail</th>
+                                    <th>File Info</th>
+                                    <th>Actions</th>
                                 </tr>
-                            @endforeach
-                        </table>
+
+                                @foreach($banners as $banner)
+                                    <tr>
+                                        <td><img src="{{$banner->url}}" style="max-height: 100px;"></td>
+                                        <td>
+                                            <b>Size:</b> {{$banner->size}}<br>
+                                            <b>Type:</b> {{$banner->type}}
+                                        </td>
+                                        <td>
+                                            <a href="{{$banner->url}}" class="btn btn-success" download="{{$banner->id}}">Download</a>
+                                            @if(Auth::user()->isAdmin())
+                                                <a href="{{route('banners.delete', $banner)}}" class="btn btn-danger">DELETE</a>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
