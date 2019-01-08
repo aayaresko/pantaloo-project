0658a996c55f4ae9e6836a1d2e9103f276536fdf
webmaxstudio <root@casinobit.ru>
2019-01-08 10:53:25 -0500

    add migrate for table trackes

---
+++ database/migrations/2019_01_08_143338_create_trackers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrackersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trackers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ref', 50);
+            $table->string('name', 50);
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('trackers');
+    }
+}
