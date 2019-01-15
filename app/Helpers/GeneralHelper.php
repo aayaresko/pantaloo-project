5e38a2fb46695357660fa99372927c6a66280bcd
alexproc <alexproc1313@gmail.com>
2019-01-15 17:28:24 +0200

    TEMPORARY TRANSALTE

--- app/Helpers/GeneralHelper.php
+++ app/Helpers/GeneralHelper.php
@@ -30,7 +30,7 @@ class GeneralHelper
     static public function getListLanguage()
     {
         $dir = '../resources/lang';
-        $languagesIndex = array_diff(scandir($dir), ['..', '.', '.gitignore']);
+        $languagesIndex = array_diff(scandir($dir), ['..', '.', '.gitignore', 'example']);
         $languages = array_values($languagesIndex);
         return $languages;
     }
