d86c7d4d327a9996593e8d403addcd2663b80d0b
Wundagor <kremen.verst@gmail.com>
2019-01-24 19:36:20 +0200

    Add language dropdown v1.1.2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <link href="/vendors/fullPage/jquery.fullPage.css" rel="stylesheet">
     <link href="/css/select2.min.css" rel="stylesheet">
     <link href="/vendors/magnific-popup/magnific-popup.css" rel="stylesheet">
-    <link href="/assets/css/languages.css" rel="stylesheet">
+    <link href="/assets/css/languages.css?v=0.0.1" rel="stylesheet">
     <link href="/css/new.css" rel="stylesheet">
     <link href="/css/main.css?v={{ time() }}" rel="stylesheet">
     <link rel="canonical" href="#" />
@@ -68,18 +68,6 @@
 <!-- header start -->
 <header class="header @if(Auth::check()) usr @endif">
     <div class="header-left">
-        <ul class="langbox">
-            <li><a href="#"><img src="{{ asset('assets/images/languages/' . app()->getLocale() . '.png') }}" alt="{{ app()->getLocale() }}" /> <span>{{ app()->getLocale() }}</span></a></li>
-            <ul class="langbox-dropdown">
-                @foreach ($languages as $language)
-                    <li>
-                        <a href="{{ url("/language/$language") }}" class="{{ (app()->getLocale() == $language) ? "active" : '' }}">
-                            <img src="{{ asset("assets/images/languages/$language.png") }}" alt="{{ $language }}" /> <span>{{ $language }}</span>
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        </ul>
         <div class="logo-block">
             <a href="/"><img src="/media/images/logo.png" alt="logo"></a>
         </div>
@@ -124,6 +112,18 @@
         @endif
     </div>
     <div class="header-right-part">
+        <ul class="langbox floated">
+            <li><a href="#"><img src="{{ asset('assets/images/languages/' . app()->getLocale() . '.png') }}" alt="{{ app()->getLocale() }}" /> <span>{{ app()->getLocale() }}</span></a></li>
+            <ul class="langbox-dropdown">
+                @foreach ($languages as $language)
+                    <li>
+                        <a href="{{ url("/language/$language") }}" class="{{ (app()->getLocale() == $language) ? "active" : '' }}">
+                            <img src="{{ asset("assets/images/languages/$language.png") }}" alt="{{ $language }}" /> <span>{{ $language }}</span>
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </ul>
         <div class="login-block floated">
             <a href="#" class="login-btn"><span class="text">{{ trans('casino.login') }}</span></a>
         </div>
