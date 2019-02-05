530000f125858fc76d9a9eb399a0a80714c59007
alexproc <alexproc1313@gmail.com>
2019-02-05 16:56:54 +0200

    add open full image

--- resources/views/admin/banner_view.blade.php
+++ resources/views/admin/banner_view.blade.php
@@ -16,7 +16,7 @@
 
                                 @foreach($banners as $banner)
                                     <tr>
-                                        <td><img src="{{$banner->url}}" style="max-height: 100px;"></td>
+                                        <td><a class = "showImage" href="#"><img src="{{$banner->url}}" style="max-height: 100px;"></a></td>
                                         <td>
                                             <b>Size:</b> {{$banner->size}}<br>
                                             <b>Type:</b> {{$banner->type}}
@@ -35,5 +35,26 @@
                 </div>
             </div>
         </div>
+
+
+        <!-- Image preview -->
+        <div class="modal fade" style = "text-align: center;" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog" style="text-align: left; max-width: 100%; width: auto !important; display: inline-block;">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Image preview</h4>
+                    </div>
+                    <div class="modal-body">
+                        <img src="" id="imagepreview">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
+
--- resources/views/admin/banners.blade.php
+++ resources/views/admin/banners.blade.php
@@ -7,3 +7,13 @@
 @section('content')
     @include('admin.banner_view', ['banners' => $banners])
 @endsection
+
+@section('js')
+    <script>
+        $(".showImage").on("click", function() {
+            let imageSrc = $(this).children().attr('src');
+            $('#imagepreview').attr('src', imageSrc);
+            $('#imagemodal').modal('show');
+        });
+    </script>
+@endsection
