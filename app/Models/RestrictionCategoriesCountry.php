53a4aa4fb6269a454de8cc3eaa8cf94b5e371a8f
alexproc <alexproc1313@gmail.com>
2019-03-18 10:41:00 +0200

    add restriction models

---
+++ app/Models/RestrictionCategoriesCountry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RestrictionCategoriesCountry extends Model
+{
+    protected $table = 'restriction_categories_by_country';
+    protected $fillable = ['category_id', 'code_country', 'mark'];
+}
---
+++ app/Models/RestrictionGamesCountry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RestrictionGamesCountry extends Model
+{
+    protected $table = 'restriction_games_by_country';
+    protected $fillable = ['game_id', 'code_country', 'mark'];
+}
