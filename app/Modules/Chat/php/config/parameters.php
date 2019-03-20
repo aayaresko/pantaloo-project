6f8640cbb45dbe7e767565ba036120570967bed9
alexproc <alexproc1313@gmail.com>
2019-03-20 17:20:09 +0200

    add chat

---
+++ app/Modules/Chat/php/config/parameters.php
@@ -0,0 +1,21 @@
+<?php
+
+return array (
+  'dbHost' => '127.0.0.1',
+  'dbPort' => 3306,
+  'dbUser' => 'root',
+  'dbPassword' => '',
+  'dbName' => 'mirrormx_customer_chat_pro',
+  'superUser' => 'admin',
+  'superPass' => '',
+  'services' => 
+  array (
+    'verify' => 
+    array (
+      'code' => '8594c015-4f42-4dcd-aa13-ab19d07c02b7',
+      'token' => 'bedefec993098ee62fcd93ba3edb2de3a8fea2ba',
+    ),
+  ),
+);
+
+?>
