edd6e7f8236fbc63efa0da21b0ae76e90d056d93
alexproc <alexproc1313@gmail.com>
2019-03-25 15:45:43 +0200

    comment code resize screen Importance

--- public/assets/js/pages/integratedGamesFullScrean.js
+++ public/assets/js/pages/integratedGamesFullScrean.js
@@ -1,42 +1,42 @@
-document.cancelFullScreen = document.cancelFullScreen || document.webkitCancelFullScreen || document.mozCancelFullScreen;
-
-function onFullScreenEnter() {
-  console.log("Enter fullscreen initiated from iframe");
-};
-
-function onFullScreenExit() {
-  console.log("Exit fullscreen initiated from iframe");
-};
-
-function enterFullscreen(id) {
-  onFullScreenEnter(id);
-  // document.getElementById('test').style.display = 'none'
-  var el =  document.getElementById(id);
-  var onfullscreenchange =  function(e){
-    var fullscreenElement = document.fullscreenElement || document.mozFullscreenElement || document.webkitFullscreenElement;
-    var fullscreenEnabled = document.fullscreenEnabled || document.mozFullscreenEnabled || document.webkitFullscreenEnabled;
-    console.log( 'fullscreenEnabled = ' + fullscreenEnabled, ',  fullscreenElement = ', fullscreenElement, ',  e = ', e);
-  }
-
-  el.addEventListener("webkitfullscreenchange", onfullscreenchange);
-  el.addEventListener("mozfullscreenchange",     onfullscreenchange);
-  el.addEventListener("fullscreenchange",             onfullscreenchange);
-
-  if (el.webkitRequestFullScreen) {
-    el.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-  } else {
-    el.mozRequestFullScreen();
-  }
-  document.querySelector('#'+id + ' button').onclick = function(){
-    exitFullscreen(id);
-  }
-}
-
-function exitFullscreen(id) {
-  onFullScreenExit(id);
-  document.getElementById('test').style.display = 'block'
-  document.cancelFullScreen();
-  document.querySelector('#'+id + ' button').onclick = function(){
-    enterFullscreen(id);
-  }
-}
+// document.cancelFullScreen = document.cancelFullScreen || document.webkitCancelFullScreen || document.mozCancelFullScreen;
+//
+// function onFullScreenEnter() {
+//   console.log("Enter fullscreen initiated from iframe");
+// };
+//
+// function onFullScreenExit() {
+//   console.log("Exit fullscreen initiated from iframe");
+// };
+//
+// function enterFullscreen(id) {
+//   onFullScreenEnter(id);
+//   // document.getElementById('test').style.display = 'none'
+//   var el =  document.getElementById(id);
+//   var onfullscreenchange =  function(e){
+//     var fullscreenElement = document.fullscreenElement || document.mozFullscreenElement || document.webkitFullscreenElement;
+//     var fullscreenEnabled = document.fullscreenEnabled || document.mozFullscreenEnabled || document.webkitFullscreenEnabled;
+//     console.log( 'fullscreenEnabled = ' + fullscreenEnabled, ',  fullscreenElement = ', fullscreenElement, ',  e = ', e);
+//   }
+//
+//   el.addEventListener("webkitfullscreenchange", onfullscreenchange);
+//   el.addEventListener("mozfullscreenchange",     onfullscreenchange);
+//   el.addEventListener("fullscreenchange",             onfullscreenchange);
+//
+//   if (el.webkitRequestFullScreen) {
+//     el.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+//   } else {
+//     el.mozRequestFullScreen();
+//   }
+//   document.querySelector('#'+id + ' button').onclick = function(){
+//     exitFullscreen(id);
+//   }
+// }
+//
+// function exitFullscreen(id) {
+//   onFullScreenExit(id);
+//   document.getElementById('test').style.display = 'block'
+//   document.cancelFullScreen();
+//   document.querySelector('#'+id + ' button').onclick = function(){
+//     enterFullscreen(id);
+//   }
+// }
