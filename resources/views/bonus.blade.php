c62e1e7b69821b96eddb957b44eb669325bada8a
alex-endorph1ne <alex.s@blockspoint.com>
2019-04-26 18:21:15 +0300

    add footer_main blade and licence link in footer

--- resources/views/bonus.blade.php
+++ resources/views/bonus.blade.php
@@ -54,38 +54,5 @@
         </div>
     </div>
 
-    <footer class="footer footer-home">
-        <div class="bitcoin-block">
-            <span class="bitcoin-msg"><i class="bitcoin-icon"></i> We work only with bitcoin</span>
-        </div>
-        <div class="msg-block">
-            <span class="msg">{{ trans('casino.do_you_want_to_play') }}</span>
-        </div>
-        <div class="games-listing-block">
-            <ul class="games-listing">
-                @include('footer_links')
-            </ul>
-        </div>
-        <div class="footer-copyrights">
-            <ul class="footerLinks">
-                <li class="rightReservedTxt">© All rights reserved</li>
-                <li><a href="{{$partnerPage}}" class="afiliate" target="_blank">{{ trans('casino.affiliates') }}</a></li>
-                <li><a target="_blank" href="{{route('support', ['lang' => $currentLang])}}" class="support">{{ trans('casino.frq') }}</a></li>
-                <li><a href="#reg-terms" class="reg-terms">{{ trans('casino.accept_the_terms_link') }}</a></li>
-                <li><a href="#uls" class="usl-link">{{ trans('casino.terms') }}</a></li>
-            </ul>
-        </div>
-        
-    </footer>
-     <div class="hidden">
-        <div id="uls">
-            {!! trans('casino.bonus.term') !!}
-        </div>
-    </div>
-
-    <div class="hidden">
-        <div id="reg-terms">
-            {!! trans('casino.terms_conditions') !!}
-        </div>
-    </div>
+    @include('footer_main')
 @endsection
