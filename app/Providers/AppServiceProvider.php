d4860b7d0b9b032a6be46891bf2a489749a22fe4
UpTechFront <taras.d@blockspoint.com>
2019-05-07 14:56:34 +0300

    add new svg icon mBIT.svg

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('FORCE_HTTPS', true)){
-            \URL::forceSchema('https');
+            // \URL::forceSchema('https');
         }
     }
 
