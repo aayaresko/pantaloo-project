c3c61346d9e1658e47e1ff3d865f081987f8d640
peresmishnyk <michkire@gmail.com>
2019-05-08 16:12:31 +0300

    no message

--- app/Console/Commands/updateUserIntercom.php
+++ app/Console/Commands/updateUserIntercom.php
@@ -2,7 +2,17 @@
 
 namespace App\Console\Commands;
 
+use App\Events\AccountStatusEvent;
+use App\Events\BonusDepositEvent;
+use App\Events\BonusGameEvent;
+use App\Events\CloseBonusEvent;
+use App\Events\DepositEvent;
+use App\Events\DepositWagerDoneEvent;
 use App\Events\OpenBonusEvent;
+use App\Events\WagerDoneEvent;
+use App\Events\WithdrawalApprovedEvent;
+use App\Events\WithdrawalFrozenEvent;
+use App\Events\WithdrawalRequestedEvent;
 use App\Providers\Intercom\Intercom;
 use App\User;
 use Illuminate\Console\Command;
@@ -40,6 +50,18 @@ class updateUserIntercom extends Command
      */
     public function handle()
     {
-        event(new OpenBonusEvent(User::findOrFail(146), 'nice test bonus'));
+        $user = User::findOrFail(146);
+
+        event(new AccountStatusEvent($user, 'old_status', 'new_status'));
+        event(new BonusDepositEvent($user, 100500));
+        event(new BonusGameEvent($user, 'game name'));
+        event(new CloseBonusEvent($user, 'bonus name'));
+        event(new DepositEvent($user, 1000));
+        event(new DepositWagerDoneEvent($user));
+        event(new OpenBonusEvent($user, 'bonus name'));
+        event(new WagerDoneEvent($user));
+        event(new WithdrawalApprovedEvent($user));
+        event(new WithdrawalFrozenEvent($user, 'comment'));
+        event(new WithdrawalRequestedEvent($user));
     }
 }
