7618f8b27baa96573e7d9f1b9ba339b9c6753eff
administrator <administrator@m2s18.solarcom.ch>
2019-05-10 12:42:21 +0200

    add cloudflare header for country check

--- app/Http/Middleware/IpCountryBlock.php
+++ app/Http/Middleware/IpCountryBlock.php
@@ -12,17 +12,21 @@ class IpCountryBlock
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-    	$ip = GeneralHelper::visitorIpCloudFire();
+        $ip = GeneralHelper::visitorIpCloudFire();
 
-        $iso_code = \geoip($ip)['iso_code'];
+        //TO DO LIBRARY OR HALPER
+        $iso_code = $_SERVER["HTTP_CF_IPCOUNTRY"];
+//        if ($iso_code == 'XX') {
+//            $iso_code = \geoip($ip)['iso_code'];
+//        }
 
-        $registrationStatus = !in_array($iso_code, ['US', 'UA', 'CA','IL']) ? 1 : 0;
+        $registrationStatus = !in_array($iso_code, ['US', 'UA', 'CA', 'IL', 'XX']) ? 1 : 0;
 
         View::share('registrationStatus', $registrationStatus);
 
