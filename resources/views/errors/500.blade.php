be5901b35892a3b5e8793bfd3175b7f2e9419f31
peresmishnyk <michkire@gmail.com>
2019-05-20 20:03:03 +0300

    no message

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,3 +1,11 @@
+@php
+
+$segment = \Illuminate\Support\Facades\Request::segment(1);
+$languages = \Illuminate\Support\Facades\Config::get('getListLanguage');
+$lang = in_array($segment, $languages) ? $segment : 'en';
+
+@endphp
+
 <div class="content">
     <div class="title">Something went wrong.</div>
 
@@ -6,7 +14,10 @@
         <script src="https://browser.sentry-cdn.com/5.2.1/bundle.min.js" crossorigin="anonymous"></script>
         <script>
             Sentry.init({ dsn: '{{ config('sentry.dsn','') }}' });
-            Sentry.showReportDialog({ eventId: '{{ app('sentry')->getLastEventId() }}' });
+            Sentry.showReportDialog({
+                eventId: '{{ app('sentry')->getLastEventId() }}',
+                lang: '{{ $lang }}'
+            });
         </script>
     @endif
 </div>
