cd3f9cb125ea42674f4c619a4f9cd57ec3ec8b7e
peresmishnyk <michkire@gmail.com>
2019-05-20 17:21:21 +0300

    fix regexp

--- resources/views/_rel_alternate.blade.php
+++ resources/views/_rel_alternate.blade.php
@@ -1,8 +1,10 @@
 @php
+    $placeholder = '::lang::';
     $langPattern = implode('|', $languages);
-
-    $relAlternateTemplate =  url(preg_replace("~^($langPattern)/~",'/::lang::/', \Illuminate\Support\Facades\Request::path()), [], \Helpers\GeneralHelper::isSecureProtocol());
+    $relAlternateTemplate =  url(preg_replace("~^($langPattern)(?=/|$)~",$placeholder, \Illuminate\Support\Facades\Request::path()), [], \Helpers\GeneralHelper::isSecureProtocol());
 @endphp
-@foreach ($languages as $language)
-    <link rel="alternate" hreflang="{{ $language }}" href="{{ str_replace('::lang::', $language, $relAlternateTemplate)  }}"/>
-@endforeach
+@if (strpos($relAlternateTemplate, $placeholder) !== false)
+    @foreach ($languages as $language)
+<link rel="alternate" hreflang="{{ $language }}" href="{{ str_replace($placeholder, $language, $relAlternateTemplate) }}"/>
+    @endforeach
+@endif
