6284ce5d09d3807a32cef577000bd15cd42334fd
vagrant <vagrant@casinobit-io>
2019-05-29 13:40:16 +0000

    blabla

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -52,6 +52,7 @@ class TestController extends Controller
     }
 
     public function error(Request $request){
+        dump(123);
         throw new Exception("Custom error!");
         return 1;
     }
