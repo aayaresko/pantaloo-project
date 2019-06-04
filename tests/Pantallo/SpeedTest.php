432ba4c3a6f385f24269eba51a204a9e51c411bf
Laravel Shift <shift@laravelshift.com>
2019-06-04 14:31:10 +0000

    Shift tests

--- tests/Pantallo/SpeedTest.php
+++ tests/Pantallo/SpeedTest.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class SpeedTest extends TestCase
+class SpeedTest extends BrowserKitTestCase
 {
     /**
      * A basic test example.
