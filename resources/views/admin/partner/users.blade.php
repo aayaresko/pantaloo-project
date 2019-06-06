1fbf38cca9860447da3ba5b2b27d07bc798032de
Garik <garik7012@gmail.com>
2019-06-06 17:06:26 +0300

    datatable global affiliate users

--- resources/views/admin/partner/users.blade.php
+++ resources/views/admin/partner/users.blade.php
@@ -30,22 +30,11 @@
                                         </thead>
 
                                         <tbody>
-                                        @foreach($players as $user)
-                                            <tr role="row">
-                                                <td>{{$user->id}}</td>
-                                                <td>{{$user->email}}</td>
-                                                <td>{{$user->created_at->format('Y-m-d')}}</td>
-                                                <td>{{$user->countries ? $user->countries->name : $user->country}}</td>
-                                                <td>{{$user->deposit() ?: 0}}</td>
-                                                <td>{{$user->todayPlayerSum()}}</td>
-                                                <td>{{$user->totalPlayerSum()}}</td>
-                                                <td>{{$user->withdraw()}}</td>
-                                            </tr>
-                                        @endforeach
+
                                         </tbody>
                                     </table>
                                 </div>
-                                {{$players->links()}}
+
                             </div>
                         </div>
                     </div>
@@ -54,4 +43,44 @@
         </div>
     </div>
 
+@endsection
+
+@section('js')
+    <script src="/adminPanel/js/general.js?v={{time()}}"></script>
+
+    <script type="text/javascript">
+        let dateStart = moment().startOf('day');
+        let dateEnd = moment().endOf('day');
+        let globalTable;
+        let optionsDefault = {};
+        let options = JSON.parse(JSON.stringify(optionsDefault));
+
+        $('#userTable').DataTable({
+            "order": [[0, "asc"]],
+            "columnDefs": [
+                {"orderable": false, "targets": [3, 4, 5, 6, 7]},
+            ],
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": '/admin/globalAffiliate/getUsersTable',
+                "dataType": "json",
+                "type": "GET",
+                "data": options,
+                "error": function (xhr, error, thrown) {
+                    console.log('error');
+                }
+            },
+            "columns": [
+                {"data": "id"},
+                {"data": "email"},
+                {"data": "created_at"},
+                {"data": "country"},
+                {"data": "deposit"},
+                {"data": "today_benefit"},
+                {"data": "total_benefit"},
+                {"data": "withdraw"},
+            ],
+        });
+    </script>
 @endsection
