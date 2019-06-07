bf8ea0db2a899036afbbb3e2b154a81ba0d839a9
alexproc <alexproc1313@gmail.com>
2019-06-07 12:30:32 +0300

    importance - all env to config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('FORCE_HTTPS', true)){
-             \Illuminate\Support\Facades\URL::forceSchema('https');
+        if (config('appAdditional.forceHttps')) {
+            \Illuminate\Support\Facades\URL::forceSchema('https');
         }
     }
 
