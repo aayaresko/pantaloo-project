de066f64db3bda05c9b5a77800306a5845f81537
peresmishnyk <michkire@gmail.com>
2019-06-10 17:28:00 +0300

    no message

--- config/database.php
+++ config/database.php
@@ -58,6 +58,20 @@ return [
             ]) : [],
         ],
 
+        'logs' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
