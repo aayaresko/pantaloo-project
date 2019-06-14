ec117a32930133b9d382d40f4cfc8d1ac9a73c7c
root <alexproc1313@gmail.com>
2019-06-14 13:27:59 +0000

    add seed for regist and logion new way

--- database/seeds/LoginTransTableSeeder.php
+++ database/seeds/LoginTransTableSeeder.php
@@ -1,16 +1,95 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LoginTransTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
-     *
-     * @return void
+     * @throws Exception
      */
     public function run()
     {
-        //
+        $currentDate = new DateTime();
+
+        DB::table('translator_translations')->insert([
+            [
+                'locale' => 'en',
+                'namespace' => '*',
+                'group' => 'casino',
+                'item' => 'not_allowed_title',
+                'text' => 'It\'s not about you...',
+                'unstable' => '0',
+                'locked' => '0',
+                'created_at' => $currentDate,
+                'updated_at' => $currentDate,
+            ],
+            [
+                'locale' => 'en',
+                'namespace' => '*',
+                'group' => 'casino',
+                'item' => 'not_allowed_subtitle',
+                'text' => 'CasinoBit.io isn\'t avaible in your country.We\'re so sorry!If you think that we were mistaken with your location, please contact us with support',
+                'unstable' => '0',
+                'locked' => '0',
+                'created_at' => $currentDate,
+                'updated_at' => $currentDate,
+            ],
+            [
+                'locale' => 'en',
+                'namespace' => '*',
+                'group' => 'casino',
+                'item' => 'affiliate_info',
+                'text' => 'You can became an affiliate thought. Just send an email to affiliates@casinobit.io',
+                'unstable' => '0',
+                'locked' => '0',
+                'created_at' => $currentDate,
+                'updated_at' => $currentDate,
+            ],
+            [
+                'locale' => 'en',
+                'namespace' => '*',
+                'group' => 'casino',
+                'item' => 'dont_have_account',
+                'text' => 'Don’t have an account?',
+                'unstable' => '0',
+                'locked' => '0',
+                'created_at' => $currentDate,
+                'updated_at' => $currentDate,
+            ],
+            [
+                'locale' => 'en',
+                'namespace' => '*',
+                'group' => 'casino',
+                'item' => 'have_account',
+                'text' => 'Have an account?',
+                'unstable' => '0',
+                'locked' => '0',
+                'created_at' => $currentDate,
+                'updated_at' => $currentDate,
+            ],
+            [
+                'locale' => 'en',
+                'namespace' => '*',
+                'group' => 'casino',
+                'item' => 'create_account',
+                'text' => 'Create',
+                'unstable' => '0',
+                'locked' => '0',
+                'created_at' => $currentDate,
+                'updated_at' => $currentDate,
+            ],
+            [
+                'locale' => 'en',
+                'namespace' => '*',
+                'group' => 'casino',
+                'item' => 'enter_account',
+                'text' => 'Sign in',
+                'unstable' => '0',
+                'locked' => '0',
+                'created_at' => $currentDate,
+                'updated_at' => $currentDate,
+            ],
+        ]);
     }
 }
