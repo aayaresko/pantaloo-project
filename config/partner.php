9b0ebdacba756b770f3377eb0fccb2ce4141e87a
peresmishnyk <michkire@gmail.com>
2019-06-19 17:03:56 +0300

    no message

--- config/partner.php
+++ config/partner.php
@@ -5,5 +5,5 @@ return [
     'casino_fit' => 18,
     'url' => env('PARTNER_ADDRESS', 'https://partner.casinobit.io'),
     'main_url' => env('MAIN_ADDRESS', 'https://casinobit.io'),
-    'addresses_to_send_confirmation' => ['email1@casinobit.io', 'email2@casinobit.io']
+    'addresses_to_send_confirmation' => ['affliates@casinobit.io', 'monica@casinobit.io']
 ];
