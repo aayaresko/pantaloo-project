8d066437f2336856ddd3106bd70265a07e4e7160
peresmishnyk <michkire@gmail.com>
2019-06-20 10:51:12 +0300

    no message

--- resources/views/robots.blade.php
+++ resources/views/robots.blade.php
@@ -1,7 +1,18 @@
 @if ($host != 'casinobit.io')
-User-agent: *
-Disallow: /
+    User-agent: *
+    Disallow: /
 @else
-User-agent: *
-Allow: /
+    User-agent: *
+    ​Disallow: */logout
+    ​Disallow: */deposit
+    ​Disallow: */bonus
+    ​Disallow: */withdraw
+    ​Disallow: */settings
+    Disallow: */password/reset
+    Disallow: /login
+    Disallow: *?*
+    Allow: *.css
+    Allow: *.js
+
+    Sitemap: https://casinobit.io/sitemap.xml
 @endif
