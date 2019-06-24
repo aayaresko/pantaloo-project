e1d319185ca7bd811a85a50ef853a63786dc0c2d
peresmishnyk <michkire@gmail.com>
2019-06-24 18:27:47 +0300

    no message

---
+++ app/Http/Middleware/UtmByRef.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class UtmByRef
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->has('ref')){
+            $url = $request->getBaseUrl() . '?utm_source=partner_a&utm_medium=affiliate&utm_campaign=' . $request->input('ref');
+            return redirect($url);
+        }
+        return $next($request);
+    }
+}
