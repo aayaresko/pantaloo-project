c1bdcc3869ed82c7723994b31e6fbf3fbc34a867
alexproc <alexproc1313@gmail.com>
2019-06-26 19:27:13 +0300

    add new fields to model withdraw

--- app/Models/Withdraw.php
+++ app/Models/Withdraw.php
@@ -8,6 +8,6 @@ class Withdraw extends Model
 {
     protected $table = 'withdraws';
 
-    protected $fillable = ['user_id', 'value', 'transaction_id', 'confirmations', 'ext_id', 'extra', 'status_withdraw'];
+    protected $fillable = ['user_id', 'value', 'transaction_id', 'to_address', 'confirmations', 'ext_id', 'extra', 'status_withdraw'];
 }
 
