ee9751114b63eca3d98d78905a12943580a3f0c9
alex-endorph1ne <alex.s@blockspoint.com>
2019-08-12 14:55:00 +0300

    new page complete

--- public/partner/js/main.js
+++ public/partner/js/main.js
@@ -10,8 +10,9 @@ function main() {
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
           if (target.length) {
             $('html,body').animate({
-              scrollTop: target.offset().top - 50
+              scrollTop: target.offset().top -30
             }, 900);
+            $('.menu-btn').removeClass('showNav')
             return false;
           }
         }
@@ -19,25 +20,16 @@ function main() {
 
 
     $('body').scrollspy({ 
-        target: '.navbar-default',
+        target: '.topNav',
         offset: 80
     });
 
-	// Hide nav on click
-  $(".navbar-nav li a").click(function (event) {
-    // check if window is small enough so dropdown is created
-    var toggle = $(".navbar-toggle").is(":visible");
-    if (toggle) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
+
 	
-	
-    // Nivo Lightbox 
-    $('.portfolio-item a').nivoLightbox({
-            effect: 'slideDown',  
-            keyboardNav: true,                            
-        });
+  $(".itemTitle").on("click", function() {
+       $(this).next().slideToggle(300);
+       $(this).find('.fa-angle-down').toggleClass("rotate")
+   })
 		
 }());
 
@@ -49,10 +41,102 @@ main();
 
 $(function(){
 
-  $(".itemTitle").on("click", function () {
-      $(this).next().slideToggle(300);
-      $(this).find('.fa-angle-down').toggleClass("rotate")
-     
-  })
+function animLogo() {                   
+  let step = 0
+  let i = 0
+  for(i; i < 5; i++){ 
+    (function(){               
+      let svgWrap = document.querySelector('.svgWrap')
+      let logo = document.getElementById('anim')
+
+      setTimeout(function() {
+        logo.classList.add("move");
+        svgWrap.style.transform = 'scaleX(1.5)'
+        logo.style.filter = 'blur(1.5px)';    
+        logo.style.transform = 'translateX(-'+step+'px)';
+        setTimeout(function() {
+          logo.classList.remove("move");
+          svgWrap.style.transform = 'scale(1)'
+          logo.style.filter = 'blur(0)';
+        }, 200)
+
+        step +=40
+
+      }, 100 + (3000*i));              
+    })(i)
+
+  }
+}
+animLogo();
+setInterval(function() {
+  animLogo() 
+}, 15000);	
+
+
+
+    $('#mainVal').on('input', function() {
+        let mainVal = $(this).val()
+
+        let valTo
+
+        if (mainVal > 20000) {
+            valTo = 165    
+
+        } else {
+            valN = mainVal / 20000  
+            valTo = (valN * 150) + 15
+            console.log(valTo);
+        }
+
+        $('.rangeCicleWrap').css('transform', 'rotate('+valTo+'deg)')
+
+        if (mainVal < 1000) {
+            calc(50)
+        } else if ((mainVal >= 1000) && (mainVal < 2000)) {
+            calc(55)
+        } else if ((mainVal >= 2000) && (mainVal < 5000)) {
+            calc(60)
+        } else if ((mainVal >= 5000) && (mainVal < 10000)) {
+            calc(65)
+        } 
+        else if (mainVal >= 10000)  {
+            calc(70)
+        } 
+
+        function calc(percent){
+            $('#gainPercent').html(percent)
+            let gainProfit = mainVal / 100 * percent
+            $('#profit').html(gainProfit.toFixed(0))
+        }
+
+    })
+
+    $(window).scroll(function(){
+     var scrollTop = $(window).scrollTop();
+     if ( scrollTop > $('#payouts').offset().top  - 200) { 
+      $('#mainVal').addClass('activeInput')
+     }
+    });
+
+
+
+    //FAQ
+    $('.question').on('click', function () {
+        $(this).parent('.faqItem').toggleClass('show')
+    })
+
+    //LANG
+    // $('.currlang').on('click', function () {
+    //     $(this).toggleClass('show')
+    // })
+
+    //MENU MOB
+    $('.menu-btn').on('click', function () {
+        $(this).toggleClass('showNav')
+        return false
+    })
+    $('.btn').on('click', function () {    
+      $('.menu-btn').removeClass('showNav')
+    })
 
 });
--- resources/views/layouts/partner.blade.php
+++ resources/views/layouts/partner.blade.php
@@ -14,21 +14,18 @@
     <link rel="apple-touch-icon" href="/partner/img/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="/partner/img/apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="/partner/img/apple-touch-icon-114x114.png">
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" type="text/css"  href="/partner/css/bootstrap.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="/partner/css/magnific-popup.css">   -->
     <link rel="stylesheet" type="text/css" href="/partner/fonts/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <!-- Bootstrap -->
+    <!-- <link rel="stylesheet" type="text/css"  href="/partner/css/bootstrap.css"> -->
+  
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet">
 
     <!-- Stylesheet
         ================================================== -->
-    <link rel="stylesheet" type="text/css" href="/partner/css/style.css?v=996739">
-    <link rel="stylesheet" type="text/css" href="/partner/css/nivo-lightbox/nivo-lightbox.css">
-    <link rel="stylesheet" type="text/css" href="/partner/css/nivo-lightbox/default.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/partner/css/style.css?v=996790">
+
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -37,48 +34,89 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top" {!! \Helpers\GeneralHelper::isTestMode() ? 'style="border:#cccc00 dashed"' : '' !!}>
-<!-- Navigation
-    ==========================================-->
-<nav id="menu" class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-            <a class="navbar-brand page-scroll" href="/" style="padding: 0 !important;"><img src="/partner/img/casinobit_partners_logo_black.svg" style="width: 190px; height: auto;" /></a> </div>
+<body id="page-top"  {!! \Helpers\GeneralHelper::isTestMode() ? 'style="border:#cccc00 dashed"' : '' !!}>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-                <!-- <li><a href="#features" class="page-scroll">Features</a></li> -->
-                <li>
-                <button type="button" class="btn popUpBtn" data-toggle="modal" data-target="#exampleModalLong">
-                Terms and Conditions</button>
-                </li>
-                <li><a href="#about" class="page-scroll">About</a></li>
-                <li><a href="#services" class="page-scroll">Services</a></li>
-                <!-- <li><a href="#portfolio" class="page-scroll">Gallery</a></li> -->
-                <li><a href="#advantages" class="page-scroll">Advantages</a></li>
-                <!--li><a href="#commissions" class="page-scroll">Commissions Structure</a></li-->
-                <li><a href="#contact" class="page-scroll">Contact</a></li>
-                <li>
-                <button class="btn btn-custom btn-lg page-scroll loginBtn" data-toggle="modal" data-target="#myModal">Login</button>
-                <button class="btn btn-custom btn-lg page-scroll" data-toggle="modal" data-target="#myModal2" style="background-image: linear-gradient(to right, #fbb05c 0%, #ff6963 100%);">Register</button>
-                </li>
-            </ul>
+
+<div class="pageWrap">
+<header class="headerWrap">
+    <div class="leftTop">			
+        <div class="logoWrap">
+            <a href="/" class="logoPc">
+                <img src="/partner/img/casinobit_logo_white_empty.svg" alt="logo">
+                <span class="svgWrap">
+                    <svg id="anim" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                        x="0px" y="0px" viewBox="0 0 1250 110" enable-background="new 0 0 1250 110" xml:space="preserve" class="">
+                        <path fill="#fff" d="M398.7,39.9c-2.6,0-5.1,0.4-7.5,1.2c8-8.9,7.2-22.6-1.7-30.6s-22.6-7.2-30.6,1.7c-7.4,8.2-7.4,20.7,0,29
+                                    c-2.4-0.8-5-1.2-7.5-1.2c-13.5,0-24.5,10.9-24.5,24.4s10.9,24.5,24.4,24.5c9.6,0,18.4-5.6,22.3-14.3c-3.7,13.8-14.3,24.8-28,28.8
+                                    l1.5,1.7H403l1.5-1.7c-13.7-4.1-24.3-15-28-28.8c5.6,12.3,20.1,17.7,32.4,12.1c12.3-5.6,17.7-20.1,12.1-32.4
+                                    C417,45.5,408.3,39.9,398.7,39.9z"></path>
+                        <path fill="#fff" d="M875,5c-3.1,8.1-10.5,18.8-20.9,29.1C843.8,44.5,833.1,51.9,825,55c8.1,3.1,18.8,10.5,29.1,20.9
+                                    C864.5,86.2,871.9,97,875,105c3.1-8.1,10.5-18.8,20.9-29.1C906.2,65.5,917,58.1,925,55c-8.1-3.1-18.8-10.5-29.1-20.9
+                                    C885.5,23.8,878.1,13.1,875,5z"></path>
+                        <path fill="#fff" d="M1147.7,52.4c6.2-3.4,9.4-8.6,9.4-15.6c0-10.7-5.9-18.2-19.5-19.9V5h-12.4v11.3c-2,0-2.9,0-9.5,0V5h-12.4
+                                    v11.3h-13.1v77.4h13.1V105h12.4V93.7h9.5V105h12.4V93.3c14.7-1.4,22.2-10,22.2-22.8C1159.8,61.3,1156.1,55.4,1147.7,52.4z
+                                    M1133.9,40.5c0,8-7.2,6.7-19.6,6.7V33.8C1128.2,33.8,1133.9,33.1,1133.9,40.5z M1114.3,75.6V61.3c13.7,0,22.1-1.3,22.1,7.1
+                                    C1136.4,77.1,1127.8,75.6,1114.3,75.6L1114.3,75.6z"></path>
+                        <path fill="#fff" d="M625,22.8c-4.9-23.7-44.4-25-53.4,3.3C557.3,70.7,615.2,84.9,625,105c9.8-20.1,67.7-34.3,53.5-78.9
+                                    C669.4-2.1,629.9-0.8,625,22.8z"></path>
+                        <path fill="#fff" d="M125,5c-8.2,17-57.2,29-45.2,66.7c7.1,22.3,36.7,22.8,44,6.5c-3.1,12.2-12.3,21.8-24.4,25.4l1.3,1.4h48.5
+                                    l1.3-1.5c-12-3.6-21.3-13.2-24.4-25.4c7.3,16.4,36.9,15.8,44-6.5C182.2,34,133.3,22,125,5z"></path>
+                    </svg>
+                </span>
+            </a>
         </div>
-        <!-- /.navbar-collapse -->
+        <a href="#" class="menu-btn"><span></span></a>
+        <div class="topNavWrap">
+            <nav class="topNav">	
+                <ul class="nav">
+                    <li class="active"><a href="#main" class="page-scroll">Main page</a></li>
+                    <li><a href="#about" class="page-scroll">About us</a></li>
+                    <li><a href="#payouts" class="page-scroll">Payment</a></li>
+                    <li><a href="#marketing" class="page-scroll">Marketing</a></li>
+                    <li><a href="#profitable" class="page-scroll">Benefits</a></li>
+                    <!-- <li><a href="#faq" class="page-scroll">FAQ</a></li> -->
+                    <li><a href="#contacts" class="page-scroll">Contact us</a></li>
+                </ul>				   
+            </nav>
+            <div class="btnWrapMob">
+                <button class="btn small empty " data-toggle="modal" data-target="#myModal">LOG IN</button>
+                <button class="btn small" data-toggle="modal" data-target="#myModal2">GET STARTED</button>
+            </div>				
+        </div>
     </div>
-</nav>
+    <div class="rightTop">
+        <div class="langSwitch">
+            <div class="currlang">
+                <img src="/partner/img/en.png" alt="">
+            </div>
+            <div class="langItems">
+                <a href="#"><img src="/partner/img/en.png" alt=""></a>
+            </div>
+        </div>
+        <div class="btnWrapPc">
+            <button class="btn small empty " data-toggle="modal" data-target="#myModal">LOG IN</button>
+            <button class="btn small" data-toggle="modal" data-target="#myModal2">GET STARTED</button>
+        </div>
+    </div>
+</header>
+
+
+
+
+
+
+
+
 
 @yield('content')
 
 <!-- Footer Section -->
-<div id="footer">
-    <div class="container text-center">
-        <p>&copy;2018-2019 CasinoBit Affiliates. Design by <a href="https://casinobit.io" rel="nofollow">CasinoBit</a></p>
-    </div>
+    <footer>
+        <span class="copyRight">© 2003-2019 CasinoBit. All right reserved.</span>
+    </footer>
 </div>
+
+
 <!-- Modal -->
 <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog">
@@ -104,14 +142,14 @@
                                             <div class="form-group">
                                                 <input type="password" name="password" tabindex="2" class="form-control" placeholder="Password" required>
                                             </div>
-                                            <div class="form-group text-center">
+                                            <div class="form-group checkWrap">
                                                 <input type="checkbox" tabindex="3" class="" name="remember_me" id="remember">
-                                                <label for="remember"> Remember Me</label>
+                                                <label for="remember" class="checkLabel"></label> <span>Remember Me</span>
                                             </div>
                                             <div class="form-group">
                                                 <div class="row">
                                                     <div class="col-sm-6 col-sm-offset-3">
-                                                        <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="btn btn-custom btn-lg page-scroll" style="margin-top: 13px;padding: 5px 15px; width: 100%;" value="Log In">
+                                                        <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="btn btn-custom btn-lg page-scroll" value="Log In">
                                                     </div>
                                                 </div>
                                             </div>
@@ -136,9 +174,7 @@
                     </div>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
+            
         </div>
     </div>
 </div>
@@ -178,7 +214,7 @@
                                             <div class="form-group">
                                                 <div class="row">
                                                     <!-- <div class="col-sm-6 col-sm-offset-3"> -->
-                                                    <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="btn btn-custom btn-lg page-scroll" style="margin-top: 13px;padding: 5px 15px; background-image: linear-gradient(to right, #fbb05c 0%, #ff6963 100%);width: 100%;" value="Register Now">
+                                                    <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="btn btn-custom btn-lg page-scroll" style="margin-top: 15px;" value="Register Now">
                                                     <!-- </div> -->
                                                 </div>
                                             </div>
@@ -194,9 +230,7 @@
                     </div>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
+           
         </div>
     </div>
 </div>
@@ -282,7 +316,7 @@
                                             </div>
                                             <div class="form-group">
                                                 <div class="row">
-                                                    <input type="submit" name="register-submit" tabindex="4" class="btn btn-custom btn-lg page-scroll" style="margin-top: 13px;padding: 5px 15px; background-image: linear-gradient(to right, #fbb05c 0%, #ff6963 100%);width: 100%;" value="Send">
+                                                    <input type="submit" name="register-submit" tabindex="4" class="btn btn-custom btn-lg page-scroll" style="margin-top: 15px;" value="Send">
                                                 </div>
                                             </div>
                                         </form>
@@ -297,9 +331,7 @@
                     </div>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
+           
         </div>
     </div>
 </div>
@@ -317,9 +349,7 @@
             <div class="modal-body">
                 <p>The message sent your our email</p>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
+            
         </div>
     </div>
 </div>
@@ -369,6 +399,7 @@
 <li>d) Will comply with all applicable rules, laws and regulations in correlation with the promotion of CasinoBit.io.</li>
 <li>e) Fully understands and accepts the Terms of the Agreement.</li>
 </ul>
+<br>
 </div>
 
 <h3 class="itemTitle">4. Responsibilities and Obligations of the Company: <i class="fas fa-angle-down"></i></h3>
@@ -403,6 +434,7 @@
 <li>f) Without prejudice to the marketing material as may be forwarded by the Company and/or made available online through the website Affiliates CasinoBit the affiliate may not use CasinoBit.io or other terms, trademarks and other intellectual property rights that are vested in the Company unless the Company consents to such use in writing.</li>
 <li>g) That “PPC and Keyword Bidding” on brand and or brand related terms is not allowed. PPC and Keyword Bidding for terms identical or similar to any of the brands, brand names, URLs (casinobit.io, casinobit.com) are strictly prohibited.</li>
 </ul>
+<br>
 </div>
 
 
@@ -529,11 +561,9 @@
     <script type="text/javascript" src="/partner/js/jquery.1.11.1.js"></script>
     <script type="text/javascript" src="/partner/js/bootstrap.js"></script>
     {{--<script type="text/javascript" src="/partner/js/SmoothScroll.js"></script>--}}
-    <script type="text/javascript" src="/partner/js/nivo-lightbox.js"></script>
     <script type="text/javascript" src="/partner/js/jqBootstrapValidation.js"></script>
     {{--<script type="text/javascript" src="/partner/js/contact_me.js"></script>--}}
-    <!-- <script src="/partner/js/jquery.magnific-popup.min.js"></script> -->
-    <script type="text/javascript" src="/partner/js/main.js?v=1"></script>
+    <script type="text/javascript" src="/partner/js/main.js?v=2"></script>
     <script type="text/javascript" src="/partner/js/leanding.js?v={{time()}}"></script>
 @show
 </body>
