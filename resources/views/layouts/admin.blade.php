0d210b2f46638358c839cbc7b5c83b0326a12718
Jeremy <58034001+networkseo@users.noreply.github.com>
2019-12-18 13:46:45 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -96,7 +96,7 @@
         <!-- LOGO -->
         <div class="topbar-left">
             <a href="/admin" class="logo"><span>Casino<span>Bit</span></span><i class="zmdi zmdi-layers"></i></a>
-   
+            <a href="/admin" class="logo"><img src="/media/images/casinobit-logo-black-379-102.png" alt="CasinoBit Logo" /></a>
         </div>
 
         <!-- Button mobile view to collapse sidebar menu -->
