408f9bf53c545118108382762f01e47185294f3d
Max S <maxs@kingd.agency>
2020-03-09 19:32:15 +0200

    Removed ip check on local

--- app/Http/Middleware/IpCountryBlock.php
+++ app/Http/Middleware/IpCountryBlock.php
@@ -4,16 +4,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Helpers\GeneralHelper;
-use Torann\GeoIP\Facades\GeoIP;
 use Illuminate\Support\Facades\View;
+use Torann\GeoIP\Facades\GeoIP;
 
 class IpCountryBlock
 {
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Closure $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -22,7 +23,13 @@ class IpCountryBlock
         $iso_code = GeneralHelper::visitorCountryCloudFlare();
         $ip = GeneralHelper::visitorIpCloudFlare();
 
-        $registrationStatus = ! in_array($iso_code, $disableRegistrationCountry) || $ip == '127.0.0.1' || GeneralHelper::isTestMode() ? 1 : 0;
+        $registrationStatus =
+            !in_array($iso_code, $disableRegistrationCountry) ||
+            $ip == '127.0.0.1' ||
+            GeneralHelper::isTestMode() ||
+            in_array(config('app.env'), ['local', 'stage'])
+                ? 1
+                : 0;
 
         View::share('registrationStatus', $registrationStatus);
 
